ed3da9a8985869b220d3e513e4cae447cb9300af
Jjake540 <77396594+Jjake540@users.noreply.github.com>
2024-04-23 18:00:01 +0100

    VER-4392: play 3 and dependencies upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,35 +3,35 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "8.4.0"
+  val bootstrapVersion = "8.5.0"
+  val mongoVersion = "1.8.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc"                  %% "bootstrap-backend-play-28"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"         % "0.71.0",
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-28" % "8.5.0",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.14.2"
+    "uk.gov.hmrc"                  %% "bootstrap-frontend-play-30" % bootstrapVersion,
+    "uk.gov.hmrc"                  %% "bootstrap-backend-play-30"  % bootstrapVersion,
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30" % bootstrapVersion,
+    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"         % mongoVersion,
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.17.0"
   )
 
   val sharedTestDependencies: Seq[ModuleID] = {
-    val scope = "test,it"
     Seq(
-      "org.scalatest"          %% "scalatest"              % "3.2.15"         % scope,
-      "org.jsoup"               % "jsoup"                  % "1.15.4"         % scope,
-      "com.typesafe.play"      %% "play-test"              % current          % scope,
-      "com.vladsch.flexmark"    % "flexmark-all"           % "0.64.6"         % scope,
-      "uk.gov.hmrc"            %% "bootstrap-test-play-28" % bootstrapVersion % scope
+      "org.scalatest"          %% "scalatest"              % "3.2.18"         % Test,
+      "org.jsoup"               % "jsoup"                  % "1.17.2"         % Test,
+      "org.playframework"      %% "play-test"              % current          % Test,
+      "com.vladsch.flexmark"    % "flexmark-all"           % "0.64.8"         % Test,
+      "uk.gov.hmrc"            %% "bootstrap-test-play-30" % bootstrapVersion % Test
     )
   }
 
   val test: Seq[ModuleID] = Seq(
-    "org.mockito"        % "mockito-core" % "5.2.0"    % Test,
-    "org.scalatestplus" %% "mockito-5-10" % "3.2.18.0" % Test
+    "org.mockito"        % "mockito-core" % "5.11.0"    % Test,
+    "org.scalatestplus" %% "mockito-5-10" % "3.2.18.0"  % Test
   )
 
   val it: Seq[ModuleID] = Seq(
-    "com.github.tomakehurst"  % "wiremock-jre8"           % "2.35.0" % IntegrationTest,
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % "0.71.0" % IntegrationTest
+    "com.github.tomakehurst"  % "wiremock-jre8"           % "3.0.1" % Test,
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % "1.8.0" % Test
   )
 
   def apply(): Seq[ModuleID] = compile ++ sharedTestDependencies ++ test ++ it
