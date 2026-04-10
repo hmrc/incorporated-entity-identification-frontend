b696595fbe73628a222fa98b1a5cfea53287c9f2
lavanya-hmrc <246458952+lavanya-hmrc@users.noreply.github.com>
2026-04-10 08:23:47 +0100

    VER-4639 - Removed ga reference from application.conf
    * Removed ga reference from application.conf
    
    * Add test script and sbt-updates plugin

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,36 +3,30 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "10.4.0"
-  val mongoVersion = "2.10.0"
+  val bootstrapVersion = "10.7.0"
+  val mongoVersion = "2.12.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"                  %% "bootstrap-backend-play-30"  % bootstrapVersion,
     "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30" % "12.20.0",
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"         % mongoVersion,
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.18.0"
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"       % "2.21.2"
   )
 
   val sharedTestDependencies: Seq[ModuleID] = {
     Seq(
-      "org.scalatest"          %% "scalatest"              % "3.2.19"         % Test,
-      "org.jsoup"               % "jsoup"                  % "1.18.1"         % Test,
-      "org.playframework"      %% "play-test"              % current          % Test,
-      "com.vladsch.flexmark"    % "flexmark-all"           % "0.64.8"         % Test,
-      "uk.gov.hmrc"            %% "bootstrap-test-play-30" % bootstrapVersion % Test
-    )
+      "org.jsoup"          % "jsoup"                  % "1.22.1",
+      "uk.gov.hmrc"       %% "bootstrap-test-play-30" % bootstrapVersion
+    ).map(_ % Test)
   }
 
   val test: Seq[ModuleID] = Seq(
-    "org.mockito"        % "mockito-core" % "5.14.0"    % Test,
-    "org.scalatestplus" %% "mockito-5-10" % "3.2.18.0"  % Test
-  )
+    "org.scalatestplus" %% "mockito-5-10" % "3.2.18.0"
+  ).map(_ % Test)
 
   val it: Seq[ModuleID] = Seq(
-    "org.wiremock"           % "wiremock"                 % "3.9.1" % Test,
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30" % mongoVersion % Test
-  )
+    "org.wiremock" % "wiremock" % "3.13.2"
+  ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ sharedTestDependencies ++ test ++ it
 
